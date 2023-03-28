532282d37f147107be70346f086975d811053b02
Charles Lee <chengche674@gmail.com>
2023-03-27 21:07:24 -0700

    uploaded previous work

---
+++ firmware/blue-pill/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <CMSIS_DSP.h>
+#include <math.h>
+#include "stm32f1xx_hal.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  // Pin definitions
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
